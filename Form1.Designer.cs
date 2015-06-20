23b194f9b08afeae55e24c3b5e28de52f6512b7e
ewa123 <ewej@wp.pl>
2015-06-20 10:49:06 +0200

    Ostatnie poprawki i koniec

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,9 +35,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(725, 287);
+            this.button1.Location = new System.Drawing.Point(174, 224);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(141, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "koniec";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,9 +45,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(679, 12);
+            this.button2.Location = new System.Drawing.Point(174, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.Size = new System.Drawing.Size(141, 30);
             this.button2.TabIndex = 1;
             this.button2.Text = "równanie Bernoulliego";
             this.button2.UseVisualStyleBackColor = true;
@@ -55,11 +55,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(679, 70);
+            this.button3.Location = new System.Drawing.Point(174, 64);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(141, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "typy rzek";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -67,8 +67,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::WindowsFormsApplication4.Properties.Resources.MTAyNHg3Njg_13352368_13352287;
-            this.ClientSize = new System.Drawing.Size(812, 322);
+            this.BackgroundImage = global::WindowsFormsApplication4.Properties.Resources.elektrownia;
+            this.ClientSize = new System.Drawing.Size(503, 259);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace WindowsFormsApplication4.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap elektrownia {
+            get {
+                object obj = ResourceManager.GetObject("elektrownia", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap MTAyNHg3Njg_13352368_13352287 {
             get {
                 object obj = ResourceManager.GetObject("MTAyNHg3Njg,13352368_13352287", resourceCulture);
